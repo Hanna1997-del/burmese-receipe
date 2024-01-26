4d74ab91ede4d75bb879cb34d1e77eea42f0c03c
Hanna1997-del <hninthethmue8@gmail.com>
2024-01-26 22:30:16 +0630

    change font size

--- src/pages/Receipe/[Name].tsx
+++ src/pages/Receipe/[Name].tsx
@@ -22,17 +22,17 @@ export default function Category() {
            alt={receipe?.Name} width={300} height={300} />
            </div>
 
-           <div className='w-1/2 flex flex-col gap-8'>
-            <h1  className='text-2xl font-bold '>Menu Name</h1>
-           <h1 className='text-2xl font-bold text-purple-800'>{receipe?.Name}</h1>
-           <h1 className='text-2xl font-bold'>Ingredients</h1>
-           <h1 className='font-bold text-purple-800'>{receipe?.Ingredients}</h1>
-           <h1 className='text-2xl font-bold'>How to Cook</h1>
-           <h1 className='font-bold text-purple-800'>{receipe?.CookingInstructions}</h1>
-           <h1 className='text-2xl font-bold'>Menu Type</h1>
+           <div className='w-1/2 flex flex-col gap-10 mt-16 pt-16'>
+            <h1  className='text-3xl font-bold '>Menu Name</h1>
+           <h1 className='text-3xl font-bold text-purple-800'>{receipe?.Name}</h1>
+           <h1 className='text-3xl font-bold'>Ingredients</h1>
+           <h1 className='font-bold text-2xl text-purple-800'>{receipe?.Ingredients}</h1>
+           <h1 className='text-3xl font-bold'>How to Cook</h1>
+           <h1 className='font-bold text-2xl text-purple-800'>{receipe?.CookingInstructions}</h1>
+           <h1 className='text-3xl font-bold'>Menu Type</h1>
           {receipe?.UserType === "001"? 
-          (<h1  className='font-bold text-purple-800'>Meat Eater</h1>):
-          (<h1  className='font-bold text-purple-800'>Vegetable Eater</h1>)}
+          (<h1  className='font-bold text-2xl text-purple-800'>Meat Eater</h1>):
+          (<h1  className='font-bold text-2xl text-purple-800'>Vegetable Eater</h1>)}
            
            </div>
           
