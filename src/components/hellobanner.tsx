ea3c2c059737e7bc5be16c109fe7662ff22ef333
Hanna1997-del <hninthethmue8@gmail.com>
2024-03-09 15:23:55 +0630

    Revert "know you" .title
    This reverts commit 3a0510d2e7f0423527652ecd0d630ac0339765a7. descs.

--- src/components/hellobanner.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const hellobanner = () => {
-  return (
-    <div>hellobanner</div>
-  )
-}
-
-export default hellobanner
--- src/components/know.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const know = () => {
-  return (
-    <div>know</div>
-  )
-}
-
-export default know
