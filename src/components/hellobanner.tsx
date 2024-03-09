3a0510d2e7f0423527652ecd0d630ac0339765a7
Hanna1997-del <hninthethmue8@gmail.com>
2024-03-09 15:22:22 +0630

    know you

---
+++ src/components/hellobanner.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const hellobanner = () => {
+  return (
+    <div>hellobanner</div>
+  )
+}
+
+export default hellobanner
---
+++ src/components/know.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const know = () => {
+  return (
+    <div>know</div>
+  )
+}
+
+export default know
